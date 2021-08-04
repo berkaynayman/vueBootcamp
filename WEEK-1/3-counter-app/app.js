f209b3b039547ece10eee6c8d505ca58e08664f7
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-04 13:20:59 +0300

    3-counter-app

--- WEEK-1/3-counter-app/app.js
+++ WEEK-1/3-counter-app/app.js
@@ -20,7 +20,25 @@ const app = Vue.createApp({
             bundan dolayı computed ile çalışırız
         */
     computed : {
-        
+        // reactiv yapıdır. reactiv olarak değişken gibi davranan, method gibi tanımlanan yapılardır.
+        getCounterResult(){
+            console.log("counter");
+            return this.counter > 5 ? "BÜYÜK" : "KÜÇÜK";
+            // çalışma mantığı: counter da bir değişim var ise kodu çalıştırır
+        },
+        getCounter2Result(){
+            console.log("counter2");
+            return this.counter2 > 5 ? "BÜYÜK" : "KÜÇÜK";
+        }
+    },
+    watch : {
+        // data veya computedd da bulunan bir bilgiyi direk izleyebilmemizi sağlar
+        counter(newValue, oldValue){
+            console.log("counterrr" ,oldValue, "=>", newValue); // counterın eski bilgisini ve yeni bilgisini alırız
+        },
+        getCounterResult(newValue, oldValue){
+            console.log("küçükbüyük", oldValue, "=>", newValue);
+        }
     }
     /*methods:{
         changePlus(){
