672dfcbba53535a067f19ac57a850853db24d729
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-04 11:37:21 +0300

    3-counter-app

--- WEEK-1/3-counter-app/app.js
+++ WEEK-1/3-counter-app/app.js
@@ -2,12 +2,25 @@ const app = Vue.createApp({
     data(){
         return{
             counter : 0,
+            counter2 : 0,
         }
     },
     methods : {
         getCounterResult(){
+            console.log("counter");
             return this.counter > 5 ? "BÜYÜK" : "KÜÇÜK";
+        },
+        getCounter2Result(){
+            console.log("counter2");
+            return this.counter2 > 5 ? "BÜYÜK" : "KÜÇÜK";
         }
+    },   /*
+            Burada getCounterResult() çalıştırdığımız zaman
+            getCounter2Result() ' de çalışır. Bu çok kötü bir durum
+            bundan dolayı computed ile çalışırız
+        */
+    computed : {
+        
     }
     /*methods:{
         changePlus(){
