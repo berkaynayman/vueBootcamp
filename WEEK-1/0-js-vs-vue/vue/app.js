d5f19695051e916a339859a4d76faf0e74516243
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-05 19:15:51 +0300

    notes add

--- WEEK-1/0-js-vs-vue/vue/app.js
+++ WEEK-1/0-js-vs-vue/vue/app.js
@@ -1,4 +1,7 @@
 const app = Vue.createApp({
+    /* vue3 vue içerisinde createApp kullanarak oluşturuz
+        ama vue2 de framwroeku kullanrak oluştururuz
+    */
     data(){
         return{
             todoText : null,
