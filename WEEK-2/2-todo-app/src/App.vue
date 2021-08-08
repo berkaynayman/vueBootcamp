e47309c0f7fe51c443cddaab9e2d604f1a954765
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-08 18:56:14 +0300

    .

---
+++ WEEK-2/2-todo-app/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h3 class="text-center"> ToDo App</h3>
+    <hr>
+    <label for="todoText"></label>
+    <input type="text" id="todoText" placeholder="Bir şeyler yazınız ...">
+    <ul>
+      <li 
+      v-for="i in 5"
+      :key="i"
+      class="d-flex justify-content-between align-items-center">
+      <span> Todo Item #{{i}}</span>
+      <button class="sm red">Sil</button>
+      </li>
+    </ul>
+  </div>
+</template>
