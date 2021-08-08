f2d34f337fd32ba298a2dec3b27d755a711c88bc
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-08 18:15:00 +0300

    vue-cli

--- WEEK-2/1-vue-cli/src/App.vue
+++ WEEK-2/1-vue-cli/src/App.vue
@@ -5,9 +5,11 @@
    <p>bugün component ve VUE CLI konularını görücez</p>
   </div>
   <CounterItem />
+  
 </template>
 
 <script>
+// bir component içerinde import edince local registiirng
 import CounterItem from "@/components/CounterItem.vue";
 export default {
   components : {
--- WEEK-2/1-vue-cli/src/main.js
+++ WEEK-2/1-vue-cli/src/main.js
@@ -1,10 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 //import "@/assets/style.css" ;
-import appHeader from "@/components/appHeader";
+import appHeader from "@/components/appHeader"; // burada import ettiğimiz için global registiring
 
 const app = createApp(App);
-
 app.component("app-header", appHeader);
-
 app.mount('#app');
