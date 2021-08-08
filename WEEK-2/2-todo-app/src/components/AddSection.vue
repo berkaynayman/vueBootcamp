dfe9c1626801eaab0e36bb5a7a2ec7b6bcc5d45e
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-09 02:24:18 +0300

    week-2/todo-app

--- WEEK-2/2-todo-app/src/components/AddSection.vue
+++ WEEK-2/2-todo-app/src/components/AddSection.vue
@@ -4,5 +4,15 @@
     <input @keydown.enter="addNewTodo" type="text" id="todoText" placeholder="Bir şeyler yazınız ...">
     -->
     <input type="text" id="todoText" placeholder="Bir şeyler yazınız ...">
-
+    <button class="green" @click="sendData">Gönder</button>
 </template>
+
+<script>
+export default {
+    methods: {
+        sendData() {
+            alert();
+        }
+    }
+};
+</script>
