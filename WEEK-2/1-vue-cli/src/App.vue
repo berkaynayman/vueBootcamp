ed641a3cd5a1d824fee41d404cc248cedf94158e
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-08 04:22:32 +0300

    vue-cli

---
+++ WEEK-2/1-vue-cli/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+   <h3> Bu benim ilk Vue CLI uygulamam</h3>
+   <p>bugün component ve VUE CLI konularını görücez</p>
+  </div>
+  <CounterItem></CounterItem>
+</template>
+
+<script>
+import CounterItem from "@/components/CounterItem.vue";
+export default {
+  components : {
+    CounterItem,
+  },
+}
+</script>
---
+++ WEEK-2/1-vue-cli/src/components/CounterItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container-sm">
+        <h3> {{ counter }}</h3>
+        <button @click="counter++">++</button>
+        <button @click="counter--">--</button>
+    </div>
+</template>
+<script>
+// bu js instancenı dış dünyaya açmam gerekiyor
+export default {
+    data(){
+        return{
+            counter :0,
+        };
+    }
+}
+</script>
