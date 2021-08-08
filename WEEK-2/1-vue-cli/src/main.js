8b6ce04653dcc5e73d6d528f84e986836d6a21e3
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-08 03:01:53 +0300

    week-2

--- WEEK-2/1-vue-cli/src/main.js
+++
@@ -1,8 +0,0 @@
-//import Vue from 'vue' ;
-import App from './App.vue' ;
-import "@/assets/style.css" ; /* @ -> /src */
-import appHeader from "@/components/appHeader";
-
-const app = createApp(App);
-app.component("app-header", appHeader);
-app.mount("#app");
