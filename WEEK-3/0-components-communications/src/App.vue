775327de2206d61166f2f71b47d1b88e7fdb5ad9
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-12 17:16:34 +0300

    WEEK-3

---
+++ WEEK-3/0-components-communications/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <userSection :ahmett="userList"/>  <!-- parent' den child' a veri göndermek yani props yöntemi -->
+    </div>
+</template>
+<script>
+import userSection from "@/components/userSection";
+export default{
+    components:{
+        userSection,
+    },
+    data(){
+        return{
+            userList:["Berkay", "Ahmet", "Temel", "Dursun"],
+        };
+    }
+
+};
+</script>
---
+++ WEEK-3/0-components-communications/src/components/userSection.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+    <ul>
+        <li v-for="user in ahmett" :key="user">{{ user }}</li>
+    </ul>
+    <!-- child' ten parent' a veri göndermek istiyorisek $emit kullanırız 
+        new-item adında event gönderiyorum değeride bu new Date().getTime()) -->
+    <button @click="$emit('new-item', new Date().getTime())">TIKLA</button>
+    </div>
+</template>
+<script>
+export default {
+    props :["ahmett"], // parentdan gelen veriyi kullanmak için belirtmeliyiz
+};
+</script>
