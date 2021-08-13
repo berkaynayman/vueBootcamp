e87143bfe724f57c85b75b4768ef7bcf23fb17c5
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-13 23:32:44 +0300

    3-http-requests

---
+++ WEEK-3/3-http-requests/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h3>Alışveriş Listesi</h3>
+    <div class="">
+      <input type="text" placeholder="alışveriş listesine ekle">
+    </div>
+    <ul>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+      <li class="d-flex justify-content-between align-items-center">
+        <span>Eleman 1</span>
+        <button class="sm red">Sil</button>
+      </li>
+    </ul>
+    <div class="text-bold text-center">5 adet öge vardır</div>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
