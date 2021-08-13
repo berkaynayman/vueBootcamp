a72b33ee89a48da67404d0f1f6790298a27a7f30
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-13 20:21:43 +0300

    WEEK-3/slot

---
+++ WEEK-3/1-component-slots/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <Modal>
+    <template #title>
+      <h3>Slot ile gelen title bilgisi</h3>
+    </template>
+
+    <template #title-content> <!-- bu syntaxda tercih edilebilr <template #title-content> -->
+      <p class="text-red">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et soluta laborum inventore quis
+          magni quibusdam, non odio ipsa recusandae tempore?</p>
+    </template>
+
+    <template #default>
+      Bu default olarak gösterileceks
+    </template>
+  </Modal>
+</template>
+<script>
+import Modal from "@/components/Modal.vue"
+export default {
+  components :{
+    Modal,
+  }
+}
+</script>
---
+++ WEEK-3/1-component-slots/src/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="header">
+        <slot name="title" />
+    </div>
+    <div class="content">
+        <slot name="title-content" />
+    </div>
+    <slot/> <!-- default için tanımladık -->
+    <div class="footer">
+      <button>Kapat</button>
+      <button>Kaydet</button>
+    </div>
+  </div>
+</template>
