6cba9c1a09f4f0ed9042036aa3d2e3c048d78766
Berkay Nayman <berkaynayman4@gmail.com>
2021-08-14 19:34:34 +0300

    week-3/action ve http

--- WEEK-3/3-http-requests/src/App.vue
+++ WEEK-3/3-http-requests/src/App.vue
@@ -1,31 +1,60 @@
 <template>
   <div class="container">
     <h3>Alışveriş Listesi</h3>
-    <div class="">
-      <input type="text" placeholder="alışveriş listesine ekle">
+    <div>
+      <input type="text" placeholder="alışveriş listesine ekle" @keydown.enter="onSave">
     </div>
-    <ul>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 1</span>
-        <button class="sm red">Sil</button>
-      </li>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 1</span>
-        <button class="sm red">Sil</button>
-      </li>
-      <li class="d-flex justify-content-between align-items-center">
-        <span>Eleman 1</span>
-        <button class="sm red">Sil</button>
+    <ul v-if="itemsList.length > 0">
+      <li v-for="item in itemsList" :key="item.id" class="d-flex justify-content-between align-items-center">
+        <span>{{ item.title }}</span>
+        <button @click="onDelete(item)" class="sm red">Sil</button>
       </li>
     </ul>
-    <div class="text-bold text-center">5 adet öge vardır</div>
+    <div v-else class="bg-red">
+        Herhangi bir ürün yok
+    </div>
+    <div class="text-bold text-center">{{itemCount}} adet öge vardır</div>
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   data(){
     return{
-
+      itemsList: []
+    };
+  },
+  mounted(){
+    axios.get("http://localhost:3000/items").then(items_response => {
+      console.log("items_response :>>", items_response);
+      this.itemsList = items_response.data || [];
+      console.log("this.itemsList :>> ", this.itemsList);
+    })
+  },
+  methods:{
+    onSave(e){
+      const saveObject = {
+        title : e.target.value,
+        created_at : new Date(),
+        completed : false
+      } ;
+      axios.post("http://localhost:3000/items", saveObject).then(save_response => {
+        console.log(save_response);
+        this.itemsList.push(save_response.data);
+        e.target.value = "";
+        e.target.focus();
+      });
+    },
+    onDelete(item){
+      axios.delete(`http://localhost:3000/items/${item.id}`).then(delete_response => {
+        console.log(delete_response);
+        this.itemsList = this.itemsList.filter(i => i.id !== item.id )
+      });
+    }
+  },
+  computed:{
+    itemCount(){
+      return this.itemsList.length
     }
   }
 }
