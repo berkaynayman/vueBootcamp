8c2fc1957c8590875abd500652372e9871636176
Berkay Nayman <berkaynayman4@gmail.com>
2021-09-05 04:56:08 +0300

    WEEK-4 59.dk

--- WEEK-4/0-vuex-introduction/src/App.vue
+++ WEEK-4/0-vuex-introduction/src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <p>
-      {{ $store.state.person }}
+      {{ $store.state.user }}
       {{ $store.state.permissions }}
 
     </p>
+    <!--
     <ul>
       <li v-for="permission in $store.state.permissions" :key="permission">{{ permission }}</li>
     </ul>
     <hr>
     <ul>
       <li v-for="user in $store.state.userList" :key="user">{{ user }}</li>
+    </ul>
+    -->
+
+    <ul>
+      <li v-for="item in woodItems" :key="item.title">{{ item.title }}</li>
     </ul>
     <hr>
     <p> {{ $store.state.fullName }} </p>
@@ -19,15 +25,16 @@
 </template>
 
 <script>
-
+import { mapGetters } from "vuex";
 export default {
   name: 'App',
   created(){
     // created() is life cycle
-    console.log(this.$store.state.person);
+    console.log(this.$store.state.user);
     console.log(this.$store.state.theme);
     /* diyelimki projemizde farklı yerlerde mobilyayı filtereleyip ekrana çekiyoruz
-       mabilya diye yazım hatası yapabiliriz. doğal olarak 1 standart elde etmek için
+       mabilya diye yazım hatası yapabiliriz. doğal olarak 
+       1- standart elde etmek için
        2- belirli kullanıcı tarafına, yazılımcıların kullanabileceği belirli başlı
        bilgileri filtreleyebilmek için state leri direk kullanıma açmak yerine getter ile
        açıyoruz. store.js içine getters{} oluşturduk. */
@@ -35,11 +42,25 @@ export default {
     
     // getters içerisindeki function çağırdık
     console.log(this.$store.getters.woodItems);
+    //console.log(this.$store.getters.activeUser);
+    console.log('this.activeUser :>> ', this.activeUser);
   },
   methods : {
     updateName(){
+      this.$store.state.itemList.push({ id : 6, title : "Sehpa", type : "mobilya" }); //burada push ettikten sonra react olduğu için list olucaktır
       this.$store.state.fullName = new Date().getTime();
-    }
+    } 
+  },
+  computed :{
+    ... mapGetters(["woodItems", "activeUser"]),
+    /*farklı syntax kullanımı
+    ...mapGetters({
+      agacElemanlari : "woodItems", agacElemanları adinda computedım var değer woodItemsdan geliyor
+      activeUser : "activeUser"
+    })
+    */
+    /*customComputed(){
+    }*/
   }
 }
 </script>
--- WEEK-4/0-vuex-introduction/src/store.js
+++ WEEK-4/0-vuex-introduction/src/store.js
@@ -2,10 +2,16 @@ import { createStore } from "vuex";
 
 const store = createStore({
     state: {
-        person: {
+        user: {
             name: "Berkay",
             lname: "Nayman",
-            age : 20 
+            age : 20,
+            address : {},
+            password : 1232323232,
+            tc : 224343
+            /* bu bilgileri diyelimki çektik ve front-end alanında gezmesini istemiyorsak
+                yazılımcının bu datayı alıp aksiyon anında tekrar backende göndermemesini istiyorsak
+                yada diyelimki passwordu silmek istiyoruzdur- getters'da activeUser tanımladık  */
         },
         theme: "dark",
         fullName : "Berkay Nayman",
@@ -22,9 +28,19 @@ const store = createStore({
     /* eğer sürekli kullandığımız birşey ise filter(mobilya gibi)
         burada getters da tamamlıyoruz */
     getters : {
-        /*benim bu store de değişken gibi davranacak yapılarım şunlardır */
+        /*benim bu store de değişken gibi davranacak yapılarım şunlardır
+            buraya bir veri gönderiririz sonra return alırız. getters reactiv çalışır
+            return olan veriyi set edemeyiz getters ın amacı return etmek  */
         /*getter tek işi state deki bilgileri alıp bana vermek */
         woodItems : state => state.itemList.filter(i => i.type === "mobilya"),
+        activeUser(state){
+            const user = {
+                ... state.user
+            };
+            delete user.password; 
+            delete user.age;
+            return user;
+        }
     }
 });
 
